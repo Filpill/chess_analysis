f53353d1fa71b6a463349e94dbf28b14a2f240f4
filpill <filiplivancic@gmail.com>
2025-04-10 15:38:46 +0000

    updating dash

--- dash/chess_dash.py
+++ dash/chess_dash.py
@@ -1,6 +1,8 @@
 import pandas_gbq
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 from datetime import date
 from dash import Dash, html, dcc, callback, dash_table
@@ -56,7 +58,7 @@ if script_setting == "extract":
 #----Development - Read Files----#
 if script_setting == "dev":
     filename = "all_data.csv"
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename, index_col=0)
 
 # Fix data types for source data
 df["game_date"] = pd.to_datetime(df["game_date"])
@@ -65,138 +67,198 @@ app = Dash(__name__)
 
 app.layout = html.Div([
 
-    html.H1([
-        html.Span(" ", className="bigger-nf-icon"),
-        html.Img(src="/assets/img/chess_logo.png", style={"width": "200px", "verticalAlign": "bottom"}),
-        " Analysis Application"
-    ], style={"textAlign": "left"}),
+    html.Div([
+        html.Div([
+                # Left side: H1 only
+                html.Div([
+                    html.H1([
+                        html.Span("", className="bigger-nf-icon", style={"marginRight": "8px"}),
+                        "Chess Analysis Application"
+                    ], style={
+                        "margin": "0"
+                    })
+                ], style={"flex": "1"}),
+
+                # Right side: image
+                html.Div([
+                    html.Img(
+                        src="/assets/img/chess_logo.png",
+                        style={"width": "175px", "height": "auto"}
+                    )
+                ], style={
+                    "display": "flex",
+                    "alignItems": "center",
+                    "justifyContent": "flex-end"
+                })
+            ], style={
+                "display": "flex",
+                "justifyContent": "space-between",
+                "alignItems": "center"
+            })
+    ], className="div-filler-outer"),
     html.Br(),
 
-    html.H3([
-        html.Span("", className="bigger-nf-icon"),
-        " Developed by ",
-        " Filip Livancic"
-    ], style={"textAlign": "left"}),
+    html.Div([
+        html.H2([
+            html.Span("󰈲", className="bigger-nf-icon"),
+            " Filters"
+        ]),
+        html.Br(),
+
+        html.Div([
+            html.Div([
+                html.Div([
+                    html.Span("󰃰", className="bigger-nf-icon", style={"marginLeft": "15px"})
+                ], style={"display": "flex", "alignItems": "", "marginRight": "0px"}),
+
+                html.H3("Date Range"),
+
+                dcc.DatePickerRange(
+                    id='date-range-picker',
+                    start_date=df['game_date'].max().date() - relativedelta(days=14),
+                    end_date=df['game_date'].max().date(),
+                    min_date_allowed=df['game_date'].min().date(),
+                    max_date_allowed=df['game_date'].max().date(),
+                    display_format='YYYY-MM-DD',
+                    style={"marginLeft": "12px"}
+                )
+            ], style={
+                'display': 'flex',
+                'alignItems': 'center',
+                'width': '30%',
+                'paddingRight': '2%',
+                'whiteSpace': 'nowrap'
+            }),
+        ]),
+
+
+        html.Div([
+
+             html.Div([
+               html.H3([
+                   html.Span("", className="bigger-nf-icon"),
+                   " Rated"
+               ]),
+
+               dcc.Dropdown(
+                   id='rated-dropdown',
+                   options=[
+                        {"label": "Unrated", "value": False},
+                        {"label": "Rated", "value": True}
+                   ],
+                   multi=True,
+                   placeholder="Select one or more",
+                   className="select-value"
+               )
+            ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%', "color": "grey"}),
+
+            html.Div([
+               html.H3([
+                   html.Span("󱚊", className="bigger-nf-icon"),
+                   " Game Rules"
+               ]),
+
+               dcc.Dropdown(
+                   id='rules-dropdown',
+                   options=[{'label': rules, 'value': rules} for rules in sorted(df['rules'].dropna().unique())],
+                   multi=True,
+                   placeholder="Select one or more",
+                   className="select-value"
+               )
+            ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%', "color": "grey"}),
+
+            html.Div([
+                html.H3([
+                    html.Span("", className="bigger-nf-icon"),
+                    " Time Class"
+                ]),
+                dcc.Dropdown(
+                    id='time-class-dropdown',
+                    options=[{'label': time_class, 'value': time_class} for time_class in sorted(df['time_class'].dropna().unique())],
+                    multi=True,
+                    placeholder="Select one or more",
+                    className="select-value"
+                )
+            ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%', "color": "grey"}),
+
+
+            html.Div([
+                html.H3([
+                    html.Span("", className="bigger-nf-icon"),
+                    " Opening"
+                ]),
+
+                dcc.Dropdown(
+                    id='opening-dropdown',
+                    options=[{'label': opening, 'value': opening} for opening in sorted(df['opening'].dropna().unique())],
+                    multi=True,
+                    placeholder="Select one or more",
+                    className="select-value"
+                )
+            ], style={'width': '20%', 'display': 'inline-block', "color": "grey"}),
+        ]),
+    ], className="div-filler-outer"),
     html.Br(),
 
-    html.H3([
-        html.Span("󰈲", className="bigger-nf-icon"),
-        " Apply Filters Here"
-    ]),
-    html.Br(),
 
     html.Div([
         html.H3([
-            html.Span("󰃰 ", className="bigger-nf-icon"),
-            " Date Range"
+            html.Span("󰄨", className="bigger-nf-icon"),
+            " Figure of Chess Games Played Data"
         ]),
-            dcc.DatePickerRange(
-                id='date-range-picker',
-                start_date=df['game_date'].max().date() - relativedelta(days=14),
-                end_date=df['game_date'].max().date(),
-                min_date_allowed=df['game_date'].min().date(),
-                max_date_allowed=df['game_date'].max().date(),
-                display_format='YYYY-MM-DD'
-            )
-        ], style={'width': '30%', 'display': 'inline-block', 'paddingRight': '2%'},  className="select-value"),
+        dcc.Graph(id="figure-game-count"),
 
+        html.Br(),
+        html.H3([
+            html.Span("󱂔", className="bigger-nf-icon"),
+            " Table of Chess Games Played Data"
+        ]),
 
-    html.Div([
+        dash_table.DataTable(
+            id='table-game-count',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            page_size=10,
+            page_action="native",
 
-         html.Div([
-           html.H3([
-               html.Span("", className="bigger-nf-icon"),
-               " Rated"
-           ]),
+            style_table={
+                'overflowX': 'auto'
+                },
 
-           dcc.Dropdown(
-               id='rated-dropdown',
-               options=[
-                    {"label": "Unrated", "value": False},
-                    {"label": "Rated", "value": True}
-               ],
-               multi=True,
-               placeholder="Select one or more"
-           )
-        ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%'}, className="select-value"), 
+            style_header={
+                'backgroundColor': '#314236',
+                "color": "white",
+                'fontWeight': 'bold'
+            },
 
-        html.Div([
-           html.H3([
-               html.Span("󱚊", className="bigger-nf-icon"),
-               " Game Rules"
-           ]),
+            style_data_conditional=[
+                {
+                    "if": {"row_index": "odd"},
+                    "backgroundColor": "#476350"
+                },
 
-           dcc.Dropdown(
-               id='rules-dropdown',
-               options=[{'label': rules, 'value': rules} for rules in sorted(df['rules'].dropna().unique())],
-               multi=True,
-               placeholder="Select one or more"
-           )
-        ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%'}, className="select-value"),
+                {
+                    "if": {"state": "selected"},
+                    "backgroundColor": "#758279",
+                    "color": "white"
+                },
 
-        html.Div([
-            html.H3([
-                html.Span(" ", className="bigger-nf-icon"),
-                " Time Class"
-            ]),
-            dcc.Dropdown(
-                id='time-class-dropdown',
-                options=[{'label': time_class, 'value': time_class} for time_class in sorted(df['time_class'].dropna().unique())],
-                multi=True,
-                placeholder="Select one or more"
-            )
-        ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%'}, className="select-value"),
+                {
+                    "if": {"state": "active"},
+                    "backgroundColor": "#758279",
+                    "color": "white"
+                }
+            ],
 
+            style_cell={
+                "backgroundColor": "#34523c",
+                "color": "white",
+                "border": "1px solid #444",
+                "padding": "10px"
+            },
 
-        html.Div([
-            html.H3([
-                html.Span("", className="bigger-nf-icon"),
-                " Opening"
-            ]),
-
-            dcc.Dropdown(
-                id='opening-dropdown',
-                options=[{'label': opening, 'value': opening} for opening in sorted(df['opening'].dropna().unique())],
-                multi=True,
-                placeholder="Select one or more"
-            )
-        ], style={'width': '20%', 'display': 'inline-block'},  className="select-value"),
-    ]),
-    html.Br(),
-
-    html.H3([
-        html.Span("󰄨", className="bigger-nf-icon"),
-        " Figure of Chess Games Played Data"
-    ]),
-    dcc.Graph(id="figure-game-count"),
-
-    html.Br(),
-    html.H3([
-        html.Span("󱂔", className="bigger-nf-icon"),
-        " Table of Chess Games Played Data"
-    ]),
-
-    dash_table.DataTable(
-        id='table-game-count',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        page_size=10,
-        page_action="native",
-        style_table={'overflowX': 'auto'},
-        style_cell={'textAlign': 'left', 'padding': '5px'},
-        style_header={
-            'backgroundColor': 'rgb(230, 230, 230)',
-            'fontWeight': 'bold'
-        },
-    )
+        )
+    ], className="div-filler-outer"),
 ],
-    #style={
-    #    "backgroundImage": 'url("https://images.chesscomfiles.com/chess-themes/backgrounds/originals/large/game_room.png")',
-    #    "backgroundSize": "cover",
-    #    "backgroundRepeat": "no-repeat",
-    #    "backgroundPosition": "center",
-    #    "height": "100vh",
-    #    "padding": "2rem"
-    #}
 )
 
 def data_filters(df, start_date, end_date, selected_rating=None, selected_rules=None, selected_time_class=None, selected_opening=None):
@@ -239,9 +301,27 @@ def data_opening_count(start_date, end_date, selected_rating, selected_rules, se
 
     # Creating Chart
     df_agg_open_count = df_filtered.groupby("game_date", as_index=False)["row_count"].sum()
-    fig = px.bar(df_agg_open_count, x="game_date", y="row_count")
-    fig.update_xaxes(tickformat="%Y %b %d", dtick="D1")
+    fig = px.bar(
+        df_agg_open_count,
+        x="game_date",
+        y="row_count",
+        color_discrete_sequence=["#609b69"]  # all bars same color
+    )
+    layout={
+        "plot_bgcolor": "#0d2611",     # chart area
+        "paper_bgcolor": "#0d2611",    # outer area
+        "font": {"color": "blue"},
+        "xaxis": {"color": "white", "gridcolor": "#444"},
+        "yaxis": {"color": "white", "gridcolor": "#444"},
+        "hoverlabel" : {
+            "font" : {"color" : "white"},     # Text color
+            "bgcolor" : "#314236",            # Background color (optional)
+            "bordercolor" :"white"            # Border color (optional)
+        },
+    }
+    fig.update_xaxes(tickformat="%b %d", dtick="D1")
     fig.update_layout(transition_duration=1000)
+    fig.update_layout(**layout)
 
     # Creating Table
     table_data = df_filtered.to_dict("records")
