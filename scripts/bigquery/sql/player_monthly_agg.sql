74b8addf8c8985dc374f65c27c8c91b999b76d9c
filpill <filiplivancic@gmail.com>
2025-04-17 15:52:35 +0000

    moved bigquery files

---
+++ scripts/bigquery/sql/player_monthly_agg.sql
@@ -0,0 +1,77 @@
+DECLARE start_date DATE DEFAULT CURRENT_DATE()-60;
+DECLARE end_date DATE DEFAULT CURRENT_DATE()-25;
+
+CREATE OR REPLACE TABLE `checkmate-453316.chess_data_aggregated.player_monthly_agg` AS (
+
+WITH cte_white_black_union AS (
+
+    SELECT  
+        DATE_TRUNC(game_date, MONTH)                       AS month_period
+      , FORMAT_DATE('%b-%y', DATE_TRUNC(game_date, MONTH)) AS formatted_month
+      , white.username                                     AS username
+      , white.rating                                       AS rating
+      , white.result                                       AS result
+      , rated
+      , time_class
+      , time_control
+      , rules
+      , accuracies.white                                   AS accuracy
+      , opening
+    FROM `checkmate-453316.chess_data.games` 
+
+      UNION ALL
+
+    SELECT  
+        DATE_TRUNC(game_date, MONTH)                       AS month_period
+      , FORMAT_DATE('%b-%y', DATE_TRUNC(game_date, MONTH)) AS formatted_month
+      , black.username                                     AS username
+      , black.rating                                       AS rating
+      , black.result                                       AS result
+      , rated
+      , time_class
+      , time_control
+      , rules
+      , accuracies.black                                   AS accuracy
+      , opening
+    FROM `checkmate-453316.chess_data.games` 
+  
+),
+
+cte_base_agg AS (
+
+    SELECT 
+        t.month_period
+      , t.formatted_month
+      , t.username
+      , t.time_class
+      , t.rules
+      , CASE
+            WHEN result = "win"                 THEN "win"
+            WHEN result = "timevsinsufficient"  THEN "draw"
+            WHEN result = "timeout"             THEN "loss"
+            WHEN result = "threecheck"          THEN "loss"
+            WHEN result = "stalemate"           THEN "draw"
+            WHEN result = "resigned"            THEN "loss"
+            WHEN result = "repetition"          THEN "draw"
+            WHEN result = "kingofthehill"       THEN "loss"
+            WHEN result = "insufficient"        THEN "draw"
+            WHEN result = "checkmated"          THEN "loss"
+            WHEN result = "bughousepartnerlose" THEN "loss"
+            WHEN result = "agreed"              THEN "draw"
+            WHEN result = "abandoned"           THEN "loss"
+            WHEN result = "50move"              THEN "draw"
+        END                              AS win_loss_draw   
+      , COUNT(*)                         AS game_count
+      , ROUND(AVG(t.rating),0)           AS avg_rating
+      , AVG(t.accuracy)                  AS avg_accuracy
+    FROM cte_white_black_union t
+
+    WHERE 1=1 
+      AND month_period BETWEEN start_date AND end_date
+      AND rated = True
+
+    GROUP BY ALL
+)
+
+SELECT * FROM cte_base_agg
+)
