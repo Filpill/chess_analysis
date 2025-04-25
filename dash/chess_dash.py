aebc18bb63d987bde6ec8978f7f138d486ad8b96
filpill <filiplivancic@gmail.com>
2025-04-25 23:00:44 +0000

    adjusting dash app for new sql table

--- dash/chess_dash.py
+++ dash/chess_dash.py
@@ -14,54 +14,43 @@ from google.auth import default
 
 from dateutil.relativedelta import relativedelta
 
-# Background URL: https://images.chesscomfiles.com/chess-themes/backgrounds/originals/large/game_room.png
-
 credentials, project_id = default()
 compute = discovery.build('compute', 'v1', credentials=credentials)
 
 script_setting = "dev"
-sql_start_date = '2025-01-01'
-sql_end_date   = '2025-01-31'
+#sql_start_date = '2025-03-01'
+sql_start_date = '2024-11-01'
+sql_end_date   = '2025-03-31'
+dataset_name = "chess_aggregate"
+table_name = "monthly_player_chess_openings"
+file_extension = ".csv"
+dev_filename = f"{table_name}{file_extension}"
+
+sql = f"""
+    SELECT  
+         *
+    FROM `checkmate-453316.{dataset_name}.{table_name}` 
+    WHERE 
+        game_month BETWEEN '{sql_start_date}' AND '{sql_end_date}' 
+    AND opening_archetype NOT IN ('ECO Not Found', 'Undefined')
+"""
 
 #----Production Query----
-if script_setting == "prod":
-    game_sql = f"""
-        SELECT  
-              game_date
-            , COUNT(*) AS opening_count
-        FROM `checkmate-453316.chess_data.games` 
-        WHERE 
-            game_date BETWEEN '{sql_start_date}' AND '{sql_end_date}' 
-        AND opening NOT IN ('ECO Not Found', 'Undefined')
-        GROUP BY ALL
-        ORDER BY 
-            opening_count DESC
-    """
-    df = pd.read_gbq(game_sql, project_id=project_id, dialect="standard", credentials=credentials)
-
-#----Extraction To Local Query----#
-if script_setting == "extract":
-    sql = """
-        SELECT  
-            *
-        FROM `checkmate-453316.chess_data_aggregated.opening_agg` 
-    """
+if script_setting in ["prod", "extract"]:
     df = pd.read_gbq(sql, project_id=project_id, dialect="standard", credentials=credentials)
 
-    #Save to CSV
-    filename = "all_data.csv"
-    df.to_csv(filename)
+    if script_setting == "extract":
+        df.to_csv(dev_filename)
 
 #-----------------------------------------------------------#
 #-----------------------------------------------------------#
 #-----------------------------------------------------------#
 #----Development - Read Files----#
 if script_setting == "dev":
-    filename = "all_data.csv"
-    df = pd.read_csv(filename, index_col=0)
+    df = pd.read_csv(dev_filename, index_col=0)
 
 # Fix data types for source data
-df["game_date"] = pd.to_datetime(df["game_date"])
+df["game_month"] = pd.to_datetime(df["game_month"])
 
 app = Dash(__name__)
 
@@ -69,7 +58,6 @@ app.layout = html.Div([
 
     html.Div([
         html.Div([
-                # Left side: H1 only
                 html.Div([
                     html.H1([
                         html.Span("", className="bigger-nf-icon", style={"marginRight": "8px"}),
@@ -79,7 +67,6 @@ app.layout = html.Div([
                     })
                 ], style={"flex": "1"}),
 
-                # Right side: image
                 html.Div([
                     html.Img(
                         src="/assets/img/chess_logo.png",
@@ -115,10 +102,10 @@ app.layout = html.Div([
 
                 dcc.DatePickerRange(
                     id='date-range-picker',
-                    start_date=df['game_date'].max().date() - relativedelta(days=14),
-                    end_date=df['game_date'].max().date(),
-                    min_date_allowed=df['game_date'].min().date(),
-                    max_date_allowed=df['game_date'].max().date(),
+                    start_date=df['game_month'].max().date() - relativedelta(days=14),
+                    end_date=df['game_month'].max().date(),
+                    min_date_allowed=df['game_month'].min().date(),
+                    max_date_allowed=df['game_month'].max().date(),
                     display_format='YYYY-MM-DD',
                     style={"marginLeft": "12px"}
                 )
@@ -133,39 +120,6 @@ app.layout = html.Div([
 
 
         html.Div([
-
-             html.Div([
-               html.H3([
-                   html.Span("", className="bigger-nf-icon"),
-                   " Rated"
-               ]),
-
-               dcc.Dropdown(
-                   id='rated-dropdown',
-                   options=[
-                        {"label": "Unrated", "value": False},
-                        {"label": "Rated", "value": True}
-                   ],
-                   multi=True,
-                   placeholder="Select one or more",
-                   className="select-value"
-               )
-            ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%', "color": "grey"}),
-
-            html.Div([
-               html.H3([
-                   html.Span("󱚊", className="bigger-nf-icon"),
-                   " Game Rules"
-               ]),
-
-               dcc.Dropdown(
-                   id='rules-dropdown',
-                   options=[{'label': rules, 'value': rules} for rules in sorted(df['rules'].dropna().unique())],
-                   multi=True,
-                   placeholder="Select one or more",
-                   className="select-value"
-               )
-            ], style={'width': '20%', 'display': 'inline-block', 'paddingRight': '2%', "color": "grey"}),
 
             html.Div([
                 html.H3([
@@ -190,7 +144,7 @@ app.layout = html.Div([
 
                 dcc.Dropdown(
                     id='opening-dropdown',
-                    options=[{'label': opening, 'value': opening} for opening in sorted(df['opening'].dropna().unique())],
+                    options=[{'label': opening, 'value': opening} for opening in sorted(df['opening_archetype'].dropna().unique())],
                     multi=True,
                     placeholder="Select one or more",
                     className="select-value"
@@ -261,25 +215,19 @@ app.layout = html.Div([
 ],
 )
 
-def data_filters(df, start_date, end_date, selected_rating=None, selected_rules=None, selected_time_class=None, selected_opening=None):
+def data_filters(df, start_date, end_date, selected_time_class=None, selected_opening=None):
 
     # Applying filters
     df_filtered = df[
-            (df['game_date'] >= pd.to_datetime(start_date)) &
-            (df['game_date'] <= pd.to_datetime(end_date))
+            (df['game_month'] >= pd.to_datetime(start_date)) &
+            (df['game_month'] <= pd.to_datetime(end_date))
         ]
-
-    if selected_rating:
-        df_filtered = df_filtered[df_filtered['rated'].isin(selected_rating)]
-
-    if selected_rules:
-        df_filtered = df_filtered[df_filtered['rules'].isin(selected_rules)]
 
     if selected_time_class:
         df_filtered = df_filtered[df_filtered['time_class'].isin(selected_time_class)]
 
     if selected_opening:
-        df_filtered = df_filtered[df_filtered['opening'].isin(selected_opening)]
+        df_filtered = df_filtered[df_filtered['opening_archetype'].isin(selected_opening)]
 
     return df_filtered
 
@@ -289,23 +237,23 @@ def data_filters(df, start_date, end_date, selected_rating=None, selected_rules=
     Output("table-game-count", "columns"),
     [Input("date-range-picker", "start_date"),
      Input("date-range-picker", "end_date"),
-     Input("rated-dropdown", "value"),
-     Input("rules-dropdown", "value"),
      Input("time-class-dropdown", "value"),
      Input("opening-dropdown", "value")]
 )
-def data_opening_count(start_date, end_date, selected_rating, selected_rules, selected_time_class, selected_opening):
+def data_opening_count(start_date, end_date, selected_time_class, selected_opening):
 
     # Applying filters
-    df_filtered = data_filters(df, start_date, end_date, selected_rating, selected_rules, selected_time_class, selected_opening)
+    df_filtered = data_filters(df, start_date, end_date, selected_time_class, selected_opening)
 
     # Creating Chart
-    df_agg_open_count = df_filtered.groupby("game_date", as_index=False)["row_count"].sum()
+    df_agg_open_count = df_filtered.groupby(["game_month", "time_class"], as_index=False)["total_games"].sum()
     fig = px.bar(
         df_agg_open_count,
-        x="game_date",
-        y="row_count",
-        color_discrete_sequence=["#609b69"]  # all bars same color
+        x="game_month",
+        y="total_games",
+        color="time_class",
+        color_discrete_sequence=px.colors.qualitative.G10 
+        #color_discrete_sequence=["#609b69"]  # all bars same color
     )
     layout={
         "plot_bgcolor": "#0d2611",     # chart area
@@ -313,6 +261,9 @@ def data_opening_count(start_date, end_date, selected_rating, selected_rules, se
         "font": {"color": "blue"},
         "xaxis": {"color": "white", "gridcolor": "#444"},
         "yaxis": {"color": "white", "gridcolor": "#444"},
+        "legend": {
+            "font" : {"color": "white"}
+        },
         "hoverlabel" : {
             "font" : {"color" : "white"},     # Text color
             "bgcolor" : "#314236",            # Background color (optional)
