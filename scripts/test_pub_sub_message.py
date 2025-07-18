dddc08a79c8d9a0a137ea5c024dd725c05e21fd9
filpill <filiplivancic@gmail.com>
2025-07-18 15:36:42 +0000

    reworking test

--- scripts/test_pub_sub_message.py
+++ scripts/test_pub_sub_message.py
@@ -3,6 +3,7 @@ import sys
 import time
 import json
 import base64
+import logging
 import traceback
 import google.cloud.logging as cloud_logging
 from google.cloud import storage
@@ -16,25 +17,30 @@ project_name = "checkmate-453316"
 logger = initialise_cloud_logger(project_name)
 
 try:
-    logger.log_text(f"Initialising Test Script For Passing In PUB/SUB MESSAGE into ENV variable")
+    logger.log_text(f"---TEST START--- Initialising Test Script For Passing In PUB/SUB MESSAGE into ENV variable")
 
     # Retrieving MESSAGE environement variable which came from pub/sub
-    #message = "{job_name : test_pub_sub_message}"
     message = os.getenv("MESSAGE")
+    #message = "{job_name : test_pub_sub_message}"
     #decoded_message = base64.b64decode(message).decode("utf-8")
 
-    if message:
-        logger.log_text(f"-----TESTING------ Recieved Pub/Sub Message: {message}" )
-        logger.log_text(f"-----TESTING------ Recieved Pub/Sub Message Type: {type(message)}" )
-    else:
-        logger.log_text(f"------TESTING------- No Message Inside Environment Variable" )
+    for i in range(5):
+        time.sleep(1)
+        if message:
+            logger.log_text(f"{i}-----TESTING------ Recieved Pub/Sub Message: {message}" )
+            logger.log_text(f"{i}-----TESTING------ Recieved Pub/Sub Message Type: {type(message)}" )
+        else:
+            logger.log_text(f"{i}------TESTING------- No Message Inside Environment Variable", severity="ERROR")
 
 except Exception as e:
-    error_message = f"-----------TESTING EXCEPTION------- Exception occurred: {str(e)}\n{traceback.format_exc()}"
-    try:
-        logger.log_text(error_message, severity="ERROR")
-    except:
-        pass  # If logger fails, don't crash again
+        error_message = f"-----------TESTING EXCEPTION------- Exception occurred: {str(e)}\n{traceback.format_exc()}"
+        try:
+            logger.log_text(error_message, severity="ERROR")
+        except:
+            pass  # If logger fails, don't crash again
 
-logging.shutdown()
+time.sleep(2)
+for handler in logging.getLogger().handlers:
+    if hasattr(handler, "flush"):
+        handler.flush()
 time.sleep(2)
