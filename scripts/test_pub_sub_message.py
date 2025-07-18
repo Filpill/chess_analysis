e0adf57cc85a66c72d91e1449ecdaf0a96f9c1ab
filpill <filiplivancic@gmail.com>
2025-07-18 01:53:13 +0000

    add error handle

--- scripts/test_pub_sub_message.py
+++ scripts/test_pub_sub_message.py
@@ -1,28 +1,37 @@
 import os
+import sys
 import json
 import base64
 import google.cloud.logging as cloud_logging
 from google.cloud import storage
+
+sys.path.append(f"../functions")
 from shared_func import initialise_cloud_logger
 
-# Initialise Logger
-project_name = "checkmate-453316"
-logger = initialise_cloud_logger(project_name)
-logger.log_text(f"Initialising Test Script For Passing In PUB/SUB MESSAGE into ENV variable")
+try:
+    # Initialise Logger
+    project_name = "checkmate-453316"
+    logger = initialise_cloud_logger(project_name)
+    logger.log_text(f"Initialising Test Script For Passing In PUB/SUB MESSAGE into ENV variable")
 
-# Appending Path To Function
-sys.path.append(f"./functions")
+    # Retrieving MESSAGE environement variable which came from pub/sub
+    message = os.getenv("MESSAGE")
+    #decoded_message = base64.b64decode(encoded_message).decode("utf-8")
 
-# Retrieving MESSAGE environement variable which came from pub/sub
-encoded_message = os.getenv("MESSAGE")
-decoded_message = base64.b64decode(encoded_message).decode("utf-8")
+    if encoded_message:
+        statement = f"-----TESTING------ Recieved Pub/Sub Message: {message}"
+        print(statement)
+        logger.log_text(statement)
 
-if encoded_message:
-    statement = f"-----TESTING------ Recieved Pub/Sub Message: {encoded_message}"
-    print(statement)
-    logger.log_text(statement)
+    else:
+        statement = f"------TESTING------- No Message Inside Environment Variable"
+        print(statement)
+        logger.log_text(statement)
 
-else:
-    statement = f"------TESTING------- No Message Inside Environment Variable"
-    print(statement)
-    logger.log_text(statement)
+except Exception as e:
+    error_message = f"Exception occurred: {str(e)}\n{traceback.format_exc()}"
+    print(error_message)
+    try:
+        logger.log_text(error_message, severity="ERROR")
+    except:
+        pass  # If logger fails, don't crash again
