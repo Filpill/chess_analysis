e2e4ffa820955730ca46719af333bb49ab07870b
filpill <filiplivancic@gmail.com>
2025-09-22 14:16:40 +0100

    successfully tested execution from GCS

---
+++ docker/gcs_python_executor/download_and_run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+BUCKET_NAME="gs://chess-deployments/*"
+DEST_DIR="/scripts"
+
+# Activate Service Account
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+
+echo "Downloading contents of ${BUCKET_NAME} → ${DEST_DIR}"
+gsutil -m cp -r "$BUCKET_NAME" "$DEST_DIR/"
+echo "Download complete"
+
+# Install python dependencies
+pip install -r /scripts/requirements.txt --break-system-packages
+
+# Run Python Script
+sleep infinity
+#python /scripts/gcs_chess_ingestion.py
